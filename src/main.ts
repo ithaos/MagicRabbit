e97be1ac21cb006d637e669704ec5be1626b311b
haos1 <haos1@ziroom.com>
2022-11-15 14:28:11 +0800

    firstfirst commitcommit

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// 引入Ele-Plus全部组件及样式
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+createApp(App)
+.use(store)
+.use(router)
+.use(ElementPlus)
+.mount('#app')
